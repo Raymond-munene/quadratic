69fe459239937c6a5006e48b610fe2e4cafbb350
Raymond-munene <55241510+Raymond-munene@users.noreply.github.com>
2021-02-14 22:31:35 +0300

    quadratic

---
+++ timetable/src/main/java/com/raymond timetable-system/MainVerticle.java
@@ -0,0 +1,12 @@
+package com.raymond timetable-system;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MainVerticle extends AbstractVerticle {
+
+    @Override
+    public void start() {
+
+    }
+
+}
